fe68c96b55204a48c599307b03a3cad25dfba6bf
Neri7711 <0272164@up.edu.mx>
2026-02-22 09:46:02 -0600

    Add files via upload

---
+++ pregunta5.sh
@@ -0,0 +1,129 @@
+    1  sudo apt update
+    2  mkdir SO
+    3  cd SO
+    4  git clone https://github.com/Robinthatdoesnotsuck/OperatingSystems2025
+    5  cls
+    6  clear
+    7  sudo apt updagrade
+    8  clear
+    9  ls
+   10  clear
+   11  ls
+   12  cd OperatingSystems2025
+   13  ls
+   14  clear
+   15  ls
+   16  cd SystemProgramming
+   17  ls
+   18  cd BasicSystemCalls
+   19  ls
+   20  cd Threads
+   21  ls
+   22  cd ..
+   23  cd PrimeraSemana
+   24  ls
+   25  cd ..
+   26  clear
+   27  ls
+   28  cd JoinExecWait
+   29  ls
+   30  cd ..
+   31  ls
+   32  clear
+   33  cd ..
+   34  ls
+   35  cd GoApiPostgres
+   36  ls
+   37  cd ..
+   38  clear
+   39  ls
+   40  mkdir clases
+   41  cd clases
+   42  clear
+   43  top
+   44  clear
+   45  top
+   46  clear
+   47  nano ejemplo.c
+   48  lw
+   49  ls
+   50  clear
+   51  top
+   52  clear
+   53  nano
+   54  clear
+   55  sudo apt install gcc
+   56  sudo dpkg --configure -a
+   57  sudo apt install gcc
+   58  apt --fix-broken install
+   59  sudo apt --fix-broken install
+   60  sudo apt install gcc
+   61  clear
+   62  ls
+   63  cd ..
+   64  ls
+   65  clear
+   66  exit
+   67  ls
+   68  cd SO
+   69  ls
+   70  cd OperatingSystems2025
+   71  ls
+   72  cd //
+   73  ;s
+   74  ls
+   75  exit
+   76  ls
+   77  cd SO
+   78  ls
+   79  clear
+   80  mkdir ejercicios
+   81  cd ejercicios
+   82  ls
+   83  clear
+   84  nano threads.cpp
+   85  clear
+   86  ls
+   87  g++ threads.cpp -o threads
+   88  ./threads
+   89  cat foo.txt
+   90  ls -l foo.txt
+   91  g++ --version
+   92  man clang
+   93  clear
+   94  ls
+   95  cd Downloads
+   96  ls
+   97  cd to
+   98  sudo apt install tor
+   99  clear
+  100  sudo apt install torbrowser-launcher\n
+  101  ls
+  102  clear
+  103  exit
+  104  mkdir investigacion
+  105  cd investigacion
+  106  nano
+  107  clear
+  108  exit
+  109  cd investigacion/; nano fuentes
+  110  nano fuentes
+  111  exit
+  112  man nmap
+  113  sudo apt update
+  114  clear
+  115  pwd
+  116  ls
+  117  cd ..
+  118  ls
+  119  cd ..
+  120  ls
+  121  exit
+  122  ls
+  123  rm -r Proyecto_SO
+  124  ls
+  125  mkdir Examen 1
+  126  cd Examen
+  127  ls
+  128  touch accept.txt; ls > accept.txt; history | grep "ls" >> accept.txt; history > pregunta5.sh
+  129  ls
